c998018f989dc30bd76dbb2cd69e8d07bdedba73
MaouiRihab <ryhabmaoui@gmail.com>
2024-10-25 14:38:55 +0100

    Add files via upload

---
+++ ClassesIncompletes/TestFinally.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class TestFinally{
+    
+ 
+
+    public static void main(String[] args) {
+     try {
+         System.out.println("entrer votre code ");
+         // traiter cette exception
+         int x = System.in.read();
+     }
+     catch(IOException e){
+         System.out.println(e);
+     }
+     
+
+        // afficher toujours le message "Merci de votre visite"
+     
+    }
+
+}
+
+
